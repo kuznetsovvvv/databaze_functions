eb986756b63e543bbb8fe002fe45a8f65154b34f
misha <mihail_160505@mail.ru>
2024-07-05 11:04:22 +0300

    second local commit

---
+++ code/build/CMakeFiles/databazefunc.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/databazefunc.dir/Functions.cpp.o"
+  "CMakeFiles/databazefunc.dir/Functions.cpp.o.d"
+  "CMakeFiles/databazefunc.dir/databaze_functions_and_files1.cpp.o"
+  "CMakeFiles/databazefunc.dir/databaze_functions_and_files1.cpp.o.d"
+  "databazefunc"
+  "databazefunc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/databazefunc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ code/build/CMakeFiles/databazefunc.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for databazefunc.
+# This may be replaced when dependencies are built.
